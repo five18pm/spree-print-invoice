c34dff40afde84c76154c5a2cc8c062ee481a3d0
five18pm <chandramohan.rangaswamy@gmail.com>
2011-07-03 23:51:05 -0700

    Updated to use Spree 0.60 style overrides

--- lib/spree_print_invoice.rb
+++ lib/spree_print_invoice.rb
@@ -7,26 +7,40 @@ module PrintInvoice
   class Engine < Rails::Engine
     
     def self.activate
-
       Admin::OrdersController.class_eval do
-        
-        show.success.wants.pdf do
+        respond_to :html, :pdf
+        respond_override :show => { :html => { :success => lambda do
           template = params[:template] || "invoice"
           render :template => "admin/orders/#{template}"  , :filename => "#{@order.number}.pdf", 
-                  :layout => false      , :content_type => "application/pdf" , :type => :erb
-        end #
-        
+                 :layout => false, :content_type => "application/pdf", :type => :erb
+          end
+        }}
+
         def method_missing(method, *args, &block)
           puts "missing #{method} has pdf=#{@pdf=!nil} args=#{args.length}"
           @pdf.respond_to?(method) ? @pdf.send(method, *args, &block) : super
-        end      
+        end
+      end
+
+      OrdersController.class_eval do
+        respond_to :html, :pdf
+        respond_override :show => { :html => { :success => lambda do
+          template = params[:template] || "invoice"
+          render :template => "admin/orders/#{template}"  , :filename => "#{@order.number}.pdf", 
+                 :layout => false, :content_type => "application/pdf", :type => :erb
+          end
+        }}
+
+        def method_missing(method, *args, &block)
+          puts "missing #{method} has pdf=#{@pdf=!nil} args=#{args.length}"
+          @pdf.respond_to?(method) ? @pdf.send(method, *args, &block) : super
+        end
       end
     end
 
 
     config.autoload_paths += %W(#{config.root}/lib)
     config.to_prepare &method(:activate).to_proc
-
   end
 end
 
